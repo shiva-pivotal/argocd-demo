cf203a04242bf51f4ef3b64b1cdec8baf9dc008b
Shiva Reddy <shreddy@pivotal.io>
2020-07-14 11:58:06 -0400

    changed hello message

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const port = 8080;
 
 console.log(process.env.ENVIRONMENT || 'e2e');
 
+if (process.env.ENVIRONMENT === 'prod') {	
+  process.exit(1);	
+}
+
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, UWM Loan Price App team!\n');
+  res.end('Hello, UWM Loan Price App team and Hello CI/CD demo!\n');
 });
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
