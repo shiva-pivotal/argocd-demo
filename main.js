5dfe6a3d44ce9127d5651066bf0e321769be1735
Shiva Baddam <sbaddam@sbaddam-a01.vmware.com>
2021-05-17 16:10:00 -0400

    Adds new change

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ if (process.env.ENVIRONMENT === 'prod') {
 const server = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Whirlpool team and Hello CI/CD demo!\n wrong one');
+  res.end('Hello, Whirlpool team and Hello CI/CD demo!\n right one');
 });
 
 server.listen(port, hostname, () => {
